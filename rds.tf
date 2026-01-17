947c711da2b04e1301a150b91b6b0ffd183ae6a1
Khalil Farhat <khalilrfarhat297@gmail.com>
2026-01-17 16:25:30 +0200

    Create rds.tf

---
+++ rds.tf
@@ -0,0 +1,16 @@
+# Provision the Oracle RDS Managed Database. (3rd Tier (Data Layer))
+
+resource "aws_db_subnet_group" "oracle_subnets" {
+  name       = "oracle-subnet-group"
+  subnet_ids = [aws_subnet.data_1.id, aws_subnet.data_2.id]
+}
+
+resource "aws_db_instance" "oracle_db" {
+  identifier           = "cloudstack-oracle"
+  engine               = "oracle-ee" # Oracle Enterprise Edition
+  instance_class       = "db.t3.small"
+  allocated_storage    = 20
+  db_subnet_group_name = aws_db_subnet_group.oracle_subnets.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+  skip_final_snapshot  = true
+}
